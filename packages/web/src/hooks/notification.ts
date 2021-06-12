3f44718640b80d451e5e1365b1afbccd147607fc
Duc-Minh Phan <alephvn@gmail.com>
2021-06-12 18:10:11 +0700

    feat(web): Add useCallback in useNotify

--- packages/web/src/hooks/notification.ts
+++ packages/web/src/hooks/notification.ts
@@ -1,18 +1,22 @@
 import { useToast } from "@chakra-ui/react";
+import { useCallback } from "react";
 
 import type { Notification } from "utils/types";
 
 const useNotify = (): ((notification: Notification) => void) => {
   const toast = useToast();
 
-  return (notification: Notification): void => {
-    toast({
-      ...notification,
-      duration: 3000,
-      isClosable: true,
-      position: "top-right",
-    });
-  };
+  return useCallback(
+    (notification: Notification): void => {
+      toast({
+        ...notification,
+        duration: 3000,
+        isClosable: true,
+        position: "top-right",
+      });
+    },
+    [toast],
+  );
 };
 
 export default useNotify;
